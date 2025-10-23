31e7ab9140fa2beac220c6191f9f4fe0895e0c36
johnBeGoode <jonathan.martin084@gmail.com>
2025-10-23 17:50:44 +0200

    Route attribute to chat function in ChatController

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -3,16 +3,18 @@
 // src/Controller/ChatController.php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Mercure\Update;
+use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mercure\HubInterface;
-use Symfony\Component\Mercure\Update;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ChatController extends AbstractController
 {
+    #[Route('/', name:'app_chat')]
     public function chat(Request $request, HubInterface $hub): Response
     {
         $form = $this->createFormBuilder()
